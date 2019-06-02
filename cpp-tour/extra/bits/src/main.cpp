406cf78987d201520e49e49e7fb67292c26d89ac
vt1 <vt7vt.dev@gmail.com>
2019-06-02 23:13:20 +0300

    bitset

---
+++ cpp-tour/extra/bits/src/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <bitset>
+#include <string>
+using namespace std;
+
+constexpr int BITS = 8;
+void show(const bitset<BITS>&);
+void flip(bitset<BITS>&);
+void flipByPos(bitset<BITS>&, int);
+bool any(bitset<BITS>&);
+
+int main()
+{	
+	int num = 102;
+	bitset<BITS> bits(num);
+	show(bits);
+	//flipByPos(bits, 4);
+	//show(bits);
+	bits <<= 1;
+	show(bits);
+	bits >>= 1;
+	show(bits);
+	bits >>= 1;
+	show(bits);	
+	bits[0] = 0;
+	bits[1] = 0;
+	bits[2] = 0;
+	bits[3] = 0;
+	bits[4] = 0;
+	bits[5] = 0;
+	bits[6] = 0;
+	bits[7] = 1;
+	if (any(bits))
+	{
+		cout << "FOund";
+	}
+	else
+	{
+		cout << "Not found";
+	}
+
+}
+
+void show(const bitset<BITS>& bits)
+{
+	unsigned long int num = bits.to_ulong();
+	string str = bits.to_string();
+	cout << "Binary: " + str << '\t' << "Number: " << num << endl;
+}
+
+void flip(bitset<BITS>& bits)
+{
+	for (int i = 0; i < bits.size(); i++)
+	{
+		bits[i] = bits[i] == 0 ? 1 : 0;
+	}
+}
+
+void flipByPos(bitset<BITS>& bits, int pos)
+{
+	bits[pos] = bits[pos] == 0 ? 1 : 0;
+}
+
+bool any(bitset<BITS>& bits)
+{
+	/*
+	for (int i = 0; i < bits.size(); i++)
+	{
+		if (bits[i] == 1)
+			return true;
+	}
+	return false;
+	*/
+	string str = bits.to_string();
+	char target = '1';
+
+	size_t found = str.find(target);
+	if (found != string::npos)
+		return true;
+	return false;
+}
